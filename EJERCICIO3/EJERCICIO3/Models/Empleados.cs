5571f1e5da612f9ae30be4988c36ecfee23f2ef4
Axel Varela <axelvarela66@gmail.com>
2022-02-13 15:02:23 -0600

    Add project files.

---
+++ EJERCICIO3/EJERCICIO3/Models/Empleados.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace EJERCICIO3.Models
+{
+   public class Empleados
+    {
+        [PrimaryKey, AutoIncrement]
+        public int codigo { get; set; }
+
+        [MaxLength(100)]
+        public String nombres { get; set; }
+
+        [MaxLength(100)]
+        public String apellidos { get; set; }
+
+        public Int32 edad { get; set; }
+
+        [MaxLength(100)]
+        public String correo { get; set; }
+
+        [MaxLength(100)]
+        public String direccion { get; set; }
+    }
+}
